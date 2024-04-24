4b216d32517cbb833413f4e88ca29958672a8774
Rafael-Csza <1492945@sga.pucminas.br>
2024-04-23 21:05:39 -0300

    codigo

---
+++ codigo-fonte/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using PUConstruir.Models;
+using System.Diagnostics;
+
+namespace PUConstruir.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Materiais()
+        {
+            return View();
+        }
+
+        public IActionResult Orcamentos()
+        {
+            return View();
+        }
+
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+
+        public IActionResult Servicos()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
