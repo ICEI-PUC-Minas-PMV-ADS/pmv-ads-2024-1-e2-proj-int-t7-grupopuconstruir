4cbb3af1edf37cc15522786a1d0ba03aec4ec291
GabrielC <gabrielcoutinhorr@outlook.com>
2024-04-27 14:44:36 -0400

    aa

--- codigo-fonte/obj/Release/net8.0/PUConstruir.AssemblyInfo.cs
+++ codigo-fonte/obj/Release/net8.0/PUConstruir.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PUConstruir")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bcbdbb0c773092861419d70fc58a60af42223ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0e094da83d60dfa2fa125323830282b366f6632")]
 [assembly: System.Reflection.AssemblyProductAttribute("PUConstruir")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PUConstruir")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
