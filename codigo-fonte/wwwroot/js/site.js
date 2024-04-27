f5c3a7a2058b9f36e43fe27e11ac887f5a0418a8
Rafael-Csza <1492945@sga.pucminas.br>
2024-04-26 23:07:03 -0300

    Login e Perfil

--- codigo-fonte/wwwroot/js/site.js
+++ codigo-fonte/wwwroot/js/site.js
@@ -1,4 +1,30 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Funções das páginas de login e cadastro.
+
+let btnLogin = document.getElementById("btnLogin");
+let btnCadastro = document.getElementById("btnCadastro");
+let btnRecuperarSenha = document.getElementById("btnRecuperarSenha");
+let blocoNome = document.getElementById("blocoNome");
+let blocoTelefone = document.getElementById("blocoTelefone");
+let tituloLogin = document.getElementById("tituloLogin");
+let popupRecuperarSenha = document.getElementById("popupRecuperarSenha");
+
+btnLogin.onclick = function () {
+    blocoNome.hidden = true;
+    blocoTelefone.hidden = true;
+    btnRecuperarSenha.hidden = false;
+    tituloLogin.innerHTML = "Login";
+    btnLogin.classList.remove("btn-disable");
+    btnCadastro.classList.add("btn-disable");
+}
+
+btnCadastro.onclick = function () {
+    blocoNome.hidden = false;
+    blocoTelefone.hidden = false;
+    btnRecuperarSenha.hidden = true;
+    tituloLogin.innerHTML = "Cadastro";
+    btnLogin.classList.add("btn-disable");
+    btnCadastro.classList.remove("btn-disable");
+}
