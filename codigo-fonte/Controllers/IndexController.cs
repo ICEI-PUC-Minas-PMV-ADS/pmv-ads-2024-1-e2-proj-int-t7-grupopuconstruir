919bb3e2d2d00b9b2c490907cb23b55d23f51bdc
Lucas Abreu <lucas.abreu.1491917@sga.pucminas.br>
2024-05-01 16:36:39 -0300

    Organizacao

---
+++ codigo-fonte/Controllers/IndexController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PUConstruir.Controllers
+{
+    public class IndexController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Material()
+        {
+            return View();
+        }
+
+        public IActionResult Orcamentos()
+        {
+            return View();
+        }
+
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+
+        public IActionResult Servicos()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ codigo-fonte/Models/MaterialModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PUConstruir.Models
+{
+    public class MaterialModel
+    {
+        public int Id { get; set; }
+        
+        public string Descricao { get; set; } = string.Empty;
+        
+        public string Um {  get; set; } = string.Empty ;
+
+        public decimal ValorPadrao { get; set; }
+
+        public decimal Altura { get; set; }
+
+        public decimal Largura { get; set; }
+
+        public decimal Comprimento { get; set; }
+
+        public decimal Peso { get; set; }
+
+        public string Cor { get; set; } = string.Empty;
+
+        public DateOnly DataCriacao { get; set; }
+
+        public string Usuario { get; set; } = string.Empty; // Fazer relacionamento com Usuário quando criar esta classe
+
+        public MaterialModel()
+        {
+            DataCriacao = DateOnly.FromDateTime(DateTime.Now);
+        }
+    }
+}
