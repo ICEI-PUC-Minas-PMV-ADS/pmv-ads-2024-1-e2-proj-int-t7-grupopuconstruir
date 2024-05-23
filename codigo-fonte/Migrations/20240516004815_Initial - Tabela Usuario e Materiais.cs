6525227f5d33619574174fbb9f843d62a8049b38
Lucas Abreu <lucas.abreu.1491917@sga.pucminas.br>
2024-05-23 20:36:52 -0300

    teste

---
+++ codigo-fonte/Migrations/20240516004815_Initial - Tabela Usuario e Materiais.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PUConstruir.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialTabelaUsuarioeMateriais : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
